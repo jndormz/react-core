df9b0ac46a023d9f95d45e99a2758fb185d828ab
jnicodormitorio <jnicodormitorio2016@gmail.com>
2019-08-19 01:23:47 +0800

    Add Products entity to database.

---
+++ 4.7.1/aspnet-core/src/DormzReactCore.Core/Products/Product.cs
@@ -0,0 +1,9 @@
+﻿using Abp.Domain.Entities;
+
+namespace DormzReactCore.Products {
+    public class Product : Entity<int> {
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
--- 4.7.1/aspnet-core/src/DormzReactCore.EntityFrameworkCore/EntityFrameworkCore/DormzReactCoreDbContext.cs
+++ 4.7.1/aspnet-core/src/DormzReactCore.EntityFrameworkCore/EntityFrameworkCore/DormzReactCoreDbContext.cs
@@ -3,13 +3,15 @@ using Abp.Zero.EntityFrameworkCore;
 using DormzReactCore.Authorization.Roles;
 using DormzReactCore.Authorization.Users;
 using DormzReactCore.MultiTenancy;
+using DormzReactCore.Products;
 
 namespace DormzReactCore.EntityFrameworkCore
 {
     public class DormzReactCoreDbContext : AbpZeroDbContext<Tenant, Role, User, DormzReactCoreDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<Product> Products { get; set; }
+
         public DormzReactCoreDbContext(DbContextOptions<DormzReactCoreDbContext> options)
             : base(options)
         {
